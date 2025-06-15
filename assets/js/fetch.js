9871a5e353b856aaa98152126eb8ed0e7e215088
kzy77 <kzy77@qq.com>
2025-06-16 00:32:12 +0800

    域名替换

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,5 +1,5 @@
 const CORS_PROXY = 'https://corsproxy.io/?';
-const RSS_URL = 'https://freshrss.chunkj.filegear-sg.me/i/?a=rss&user=chunkj&token=chunkj&hours=24';
+const RSS_URL = 'https://freshrss.chunkj.dpdns.org/i/?a=rss&user=chunkj&token=chunkj&hours=24';
 
 async function fetchRSSFeeds() {
   try {
